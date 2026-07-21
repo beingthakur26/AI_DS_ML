797f11726110ebbde0334fc34af88c541da2cd49
Manas Singh <singhmanas2918@gmail.com>
2026-07-21 13:47:31 +0530

    genAI-2-part6

--- GenerativeAI by SheryainAI/RAG-Project/documents-loader/text.py
+++ GenerativeAI by SheryainAI/RAG-Project/documents-loader/text.py
@@ -1,6 +1,22 @@
 from langchain_community.document_loaders import TextLoader
 
+# text_splitter: text splitter to split the text into chunks
+# chunks are used to create embeddings and store in vector database
+from langchain_text_splitters import CharacterTextSplitter
+
+splitter = CharacterTextSplitter(
+    separator="",
+    chunk_size=10,
+    chunk_overlap=1
+)
+
 data = TextLoader("documents-loader/notes.txt")
 docs = data.load()
 
-print(docs)
+chunks = splitter.split_documents(docs)
+
+# print(docs)
+# print(len(chunks))
+for i in chunks:    
+    print(i.page_content)
+    print("--------------------------------------------------")
