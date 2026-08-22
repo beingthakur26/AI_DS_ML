e5cd6118aad086339ecbef7fd8c708b61f2eb814
Manas Singh <singhmanas2918@gmail.com>
2026-08-22 20:47:28 +0530

    done

--- GenerativeAI by SheryainAI/RAG-Project/documents-loader/pdf.py
+++ GenerativeAI by SheryainAI/RAG-Project/documents-loader/pdf.py
@@ -30,7 +30,7 @@ data = PyPDFLoader("documents-loader/GRU.pdf")
 docs = data.load()
 
 splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, 
+    chunk_size=1000,  
     chunk_overlap=10
 )
 chunks = splitter.split_documents(docs)
